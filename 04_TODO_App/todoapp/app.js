8b539c06d9dd488a163c5e13de7248d427dbe9b1
Anmol Tomer <anmol3540@gmail.com>
2020-07-09 11:02:22 +0530

    Add mobile view support.

--- 04_TODO_App/todoapp/app.js
+++ 04_TODO_App/todoapp/app.js
@@ -37,7 +37,7 @@ MongoClient.connect(url, {
     console.log("Connected successfully to server on url " + url);
     const db = client.db('todoapp')
     Todos = db.collection("todos");
-    app.listen(port, () => {
+    app.listen(port, '0.0.0.0', () => {
         console.log('Server running on port ' + port);
     });
 
