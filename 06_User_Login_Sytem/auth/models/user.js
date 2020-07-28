4553e39fb0ead007da3368c0cb5e801f426f4c2f
Anmol Tomer <anmol3540@gmail.com>
2020-07-29 00:41:12 +0530

    Add models to be used with Mongoose and stuck on register reloading error.

---
+++ 06_User_Login_Sytem/auth/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/passportapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const bcrypt = require('bcryptjs')
+
+// user schema
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+
+    },
+    username: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+
+    },
+    password: {
+        type: String,
+
+    },
+
+})
+
+// Create var called user and set it to module.exports
+
+const User = module.exports = mongoose.model('User', userSchema)
+
+module.exports.register = function (newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+                console.log(err)
+            }
+            newUser.password = hash
+            newUser.save(callback)
+        })
+    })
+}
